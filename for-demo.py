eb046d89288f1d2f6ab48a4c3455642b4bf21c0c
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:42:13 +0300

    Create for-demo.py

---
+++ for-demo.py
@@ -0,0 +1,20 @@
+sayilar = [1,3,5,7,9,12,19,21]
+
+# 1- for sayi in sayilar:
+#     if sayi%3 == 0:
+#         print(sayi)
+
+# 2- toplam = 0
+# for sayi in sayilar:
+#     toplam += sayi
+# print("toplam:",toplam)
+
+# 3- for sayi in sayilar:
+#     if (sayi % 2 == 1):
+#         print(sayi ** 2)
+
+sehirler = ['kocaeli','istanbul','ankara','rize','izmir']
+
+# 4- for sehir in sehirler:
+#     if len(sehir) <= 5:
+#         print(sehir)
