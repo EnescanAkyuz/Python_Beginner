9e79e976d0cd33539de673db19f506a0f9f5afb7
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:41:37 +0300

    Create asal-sayi.py

---
+++ asal-sayi.py
@@ -0,0 +1,17 @@
+sayi = int(input("sayı: "))
+asalmi = True
+
+if sayi == 1:
+    asalmi = False
+
+for i in range(2,sayi):
+    if sayi % i == 0:
+        asalmi = False
+        break
+    
+if asalmi:
+    print("sayı asal.")
+else:
+    print("sayı asal değil.")
+
+
