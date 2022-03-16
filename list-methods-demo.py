f883d4d9477cb7d57f936b67b1b834bc18b1b6ad
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:42:24 +0300

    Create list-methods-demo.py

---
+++ list-methods-demo.py
@@ -0,0 +1,28 @@
+name = ["Ali", "Yağmur", "Hakan", "Deniz"]
+years = [1998, 2000, 1998, 1987]
+
+# 1 name.append("Cenk")
+# 2 name.insert(0, "Sena")
+# 3 name.pop(3)
+# 4
+# 5 name = "Ali" in name
+# 6 okey
+# 7 name.sort()
+# 8 years.sort()
+# str = "Chevrolet,Dacia"
+# 10 years = max(years)
+# years = years.count(1998)
+# 12 years.clear() 
+# markalar = []
+# marka = input('marka: ')
+# markalar.append(marka)
+
+# marka = input('marka: ')
+# markalar.append(marka)
+
+# marka = input('marka: ')
+# markalar.append(marka)
+
+
+# print(markalar)
+
