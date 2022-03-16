3614bff6ab3222321661ea65eb8c17997c740537
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:41:51 +0300

    Create comparison.py

---
+++ comparison.py
@@ -0,0 +1,33 @@
+# a = int(input("a : "))
+# b = int(input("b : "))
+# sonuc = (a > b)
+# print(f'a: {a} b: {b} den büyüktür : {sonuc} ')
+
+
+# a = float(input("1.not(vize): "))
+# b = float(input("2.not(vize): "))
+# c = float(input("1.not(final): "))
+# d = float(input("2.not(final): "))
+# ortalama = ((a + b) / 2) * (60 / 100) + ((c + d) / 2) * (40 / 100) 
+# result = (ortalama >= 50)
+
+# print(f"geçti : {result}") 
+
+
+# sayi = int(input("sayı: "))
+# sonuc = (sayi % 2 == 0)
+# print(f"çift olma durumu : {sonuc}")
+
+# sayi = int(input("sayı : "))
+# pozitif = ( sayi > 0)
+# negatif = ( sayi < 0)
+# print(f"verdiğiniz sayının pozitif olma durumu: {pozitif}")
+
+
+email = 'enes@gmail.com' 
+password = 'enes123'
+a = input("email: ")
+b = input("password: ")
+basarılı1 = (email == a) 
+basarılı2 = (password == b)
+print(f"girişiniz başarı durumu: {basarılı1} {basarılı2}")
