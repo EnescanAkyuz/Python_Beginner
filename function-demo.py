f90905a6a983e702e69c1b3a9075538d24558ec2
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:42:16 +0300

    Create function-demo.py

---
+++ function-demo.py
@@ -0,0 +1,55 @@
+# def yazdir(kelime, adet):
+#     print(kelime * adet)
+
+# yazdir('enes', 5)
+
+
+
+
+# def listeye(*params):
+#     liste = []
+
+#     for param in params:
+#         liste.append(param)
+#     return liste
+
+# result = listeye(10,20,'enes')
+# print(result)
+
+
+
+# sayi1 = int(input("sayı: "))
+# sayi2 = int(input("sayı: "))
+
+# def asalbul(sayi1, sayi2):
+#     for a in range(sayi1,sayi2+1):
+#         if a>1:
+#             for i in range(2,a):
+#                 if a%i==0:
+#                     break
+#             else:
+#                 print(a) 
+            
+
+# asalbul(sayi1, sayi2)
+
+
+
+
+
+# def tambolen(sayi):
+#     liste = []
+
+#     for num in range(1,sayi):
+        
+#         if sayi%num==0:
+#             liste.append(num)
+            
+#     else:
+#         print(sayi)  
+#     return liste     
+
+    
+# result = tambolen(10)
+# print(result)
+
