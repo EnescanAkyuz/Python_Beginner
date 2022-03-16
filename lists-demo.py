78d1f1382c463b1b0973ea8cd8e1ea31e722dd51
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:42:26 +0300

    Create lists-demo.py

---
+++ lists-demo.py
@@ -0,0 +1,19 @@
+cars = ['BMW', 'Mercedes', 'Opel', 'Mazda']
+# 2 print(len(cars))
+# 3 print(cars[0])
+# 3 print(cars[3])
+# 4 cars[3] = 'Toyota'
+# 5 cars = 'Mercedes' in cars
+# 6 print(cars[-2])
+# 7 print(cars[0:3])
+# 8 cars[2] = 'Toyota'
+# 8 cars[3] = 'Renault'
+# 9 cars = cars + ['Audi', 'Nissan']
+# 10 cars.remove('Mazda')
+# 10 cars.pop(3)
+# 11 print(cars[::-1])
+studentA = ["Yiğit Bilgi 2010, (70,60,70)"]
+studentB = ["Sena Turan 1999, (80,80,70)"]
+studentC = ["Ahmet Turan 1998, (80,70,90)"]
+result = studentA + studentB + studentC
+print(result)
