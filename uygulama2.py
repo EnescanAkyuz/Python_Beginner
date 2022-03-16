bdf7dd89c12d77c305a29d7a150755d27b71ec5b
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:43:14 +0300

    Create uygulama2.py

---
+++ uygulama2.py
@@ -0,0 +1,20 @@
+website = "http://www.sadikturan.com"
+course = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+message = ' Hello World '
+# message = message.strip()
+# message = message.lower()
+# website = website.count('a')
+# isFound = website.startswith('www')
+# isFound = website.endswith('com')
+# isFound = website.find('.com')
+# isalpha = course.isalpha()
+# isalpha = course.isdigit()
+# s = 'contents'
+# s = s.center(50, '*')
+# course = course.replace(' ', '-')
+# message = message.replace('World', 'There')
+course = course.split()
+
+
+print(course)
