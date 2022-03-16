61fafc94ce95a9680da31b768f61c87c308f2075
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:43:12 +0300

    Create uygulama.py

---
+++ uygulama.py
@@ -0,0 +1,21 @@
+website = "http://www.sadikturan.com"
+course = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+# print(len(course)) (65)
+# print(website[7:10]) okey 
+# print(website[22:25]) okey 
+result = course[:15]
+result = course[-15:]
+# ffdk
+# result = course[::-1]
+name = 'Bora'
+surname = 'YILMAZ'
+age = '32'
+jobs = 'Mühendis'
+# result = "Benim adım "+ name+ " "+ surname+ ", Yaşım "+ age+ " ve işim "+ jobs
+# result = f"Benim adım {name} {surname} , yaşım {age} ve mesleğim {jobs}"
+# result = "Benim adım {} {} , yaşım {} ve mesleğim {}" .format(name, surname, age , jobs)
+s = 'abc' * 3
+print(s)
+
+
