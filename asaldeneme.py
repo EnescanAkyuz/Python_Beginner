5d66f5b3c978f8686fa40fbe82762c085d849288
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:41:39 +0300

    Create asaldeneme.py

---
+++ asaldeneme.py
@@ -0,0 +1,14 @@
+for i in range(1,122):
+    if i > 1:    
+        for j in range(2,i):
+            if i % j == 0:
+                break                
+        else:
+            print(i)
+
+
+
+       
+
+
+
