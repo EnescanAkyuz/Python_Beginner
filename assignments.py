5a69dd77bd1bd77ddd3eb2de9992c351a94390c7
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:41:41 +0300

    Create assignments.py

---
+++ assignments.py
@@ -0,0 +1,15 @@
+x, y, z = 2, 5, 10
+
+# sayı1 = input("1.sayı : ")
+# sayı2 = input("2.sayı : ")
+# carpım = int(sayı1) * int(sayı2)
+# toplam = x + y + z
+# print(toplam - carpım)
+
+print(y // x)
+toplam = x + y + z
+print(toplam % 3)
+print(y ** x)
+numbers = 1, 5, 7, 10, 6
+x, *y, z = numbers
+print(z ** 3)
